a18b983f26c3f52087612abb445db68916a7f17b
Bluecraft <91048594+Bluecraftyt@users.noreply.github.com>
2021-12-27 19:50:46 +0000

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,60 +1,224 @@
-const { REST } = require("@discordjs/rest"); // Define REST.
-const { Routes } = require("discord-api-types/v9"); // Define Routes.
-const fs = require("fs"); // Define fs (file system).
-const { Client, Intents, Collection } = require("discord.js"); // Define Client, Intents, and Collection.
+const { Client, Collection } = require("discord.js");
+const { config } = require("dotenv");
+const { prefix, db } = require("./config.json");
+require("./server.js");
+const Discord = require("discord.js");
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-}); // Connect to our discord bot.
-const commands = new Collection(); // Where the bot (slash) commands will be stored.
-const commandarray = []; // Array to store commands for sending to the REST API.
-const token = process.env.DISCORD_TOKEN; // Token from Railway Env Variable.
-// Execute code when the "ready" client event is triggered.
-client.once("ready", () => {
-  const commandFiles = fs
-    .readdirSync("src/Commands")
-    .filter(file => file.endsWith(".js")); // Get and filter all the files in the "Commands" Folder.
-
-  // Loop through the command files
-  for (const file of commandFiles) {
-    const command = require(`./Commands/${file}`); // Get and define the command file.
-    commands.set(command.data.name, command); // Set the command name and file for handler to use.
-    commandarray.push(command.data.toJSON()); // Push the command data to an array (for sending to the API).
-  }
-
-  const rest = new REST({ version: "9" }).setToken(token); // Define "rest" for use in registering commands
-  // Register slash commands.
-  ;(async () => {
-    try {
-      console.log("Started refreshing application (/) commands.");
-
-      await rest.put(Routes.applicationCommands(client.user.id), {
-        body: commandarray,
-      });
-
-      console.log("Successfully reloaded application (/) commands.");
-    } catch (error) {
-      console.error(error);
-    }
-  })();
-  console.log(`Logged in as ${client.user.tag}!`);
+  disableEveryone: true
 });
-// Command handler.
-client.on("interactionCreate", async interaction => {
-  if (!interaction.isCommand()) return;
+//--------WELCOME---------
+const buttons = require("discord-buttons")(client);
+const { createCanvas, loadImage, registerFont } = require("canvas");
+//--------MUSIC - CLIENT------
+const { Player } = require('discord-player');
+const fs = require("fs")
+client.player = new Player(client);
 
-  const command = commands.get(interaction.commandName);
+//-----database-------
+const {Database} = require("quickmongo")
+//Collection
+client.db =  new Database(db)
+client.commands = new Collection();
+client.aliases = new Collection();
+client.queue = new Map();
+client.snipes = new Map();
 
-  if (!command) return;
-
-  try {
-    await command.execute(interaction, client);
-  } catch (error) {
-    console.error(error);
-    return interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
-  }
+["command", "event"].forEach(handler => {
+  require(`./handlers/${handler}`)(client);
 });
 
-client.login(token); // Login to the bot client via the defined "token" string.
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(console.log("Connected to mongo db"));
+
+
+
+//RANKS
+const Levels = require("discord-xp");
+Levels.setURL(db);
+
+
+
+
+
+
+
+
+
+
+
+
+client.on("ready", () => {
+  console.log(`Hi, ${client.user.username} is now online!`);
+
+  client.user.setActivity("!help");
+});
+
+
+client.on("clickMenu",async menu => {
+
+if(menu.values[0]== "APPLE"){
+
+
+
+	menu.channel.send(`I ALSO LIKE APPLE`)
+}
+
+
+if(menu.values[1]== "MANGO"){
+
+
+
+	menu.channel.send(`I ALSO LIKE MANGO`)
+
+
+}
+if(menu.values[3]== "BANANA"){
+
+
+
+	menu.channel.send(`I ALSO LIKE BANANA`)
+}
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on("messageDelete",async (message) => {
+
+
+
+
+  client.snipes.set(message.channel.id,{
+content : message.content,
+author: message.author.tag,
+image: message.attachments.first() ? message.attachments.first().proxyURL : null
+
+
+
+
+  })
+})
+
+
+
+//ANTI-MENTION
+client.on("message",async message => {
+  
+if(message.mentions.members.array().length >= 4){
+  
+ if(message){
+   await  message.delete()
+ } 
+return message.reply(`You are not allow to mention mass members!`) 
+  
+}
+ 
+  
+  
+})
+
+//THANKS FOR WATCHING 
+//FOR MORE HELP JOIN MY SERVER 
+
+
+
+
+
+
+client.on("message", async message => {
+  if (message.author.bot) return;
+  if (!message.guild) return;
+
+
+
+        const randomXp = Math.floor(Math.random() * 98) + 1;
+        const level = await Levels.appendXp(
+          message.author.id,
+          message.guild.id,
+          randomXp
+        );
+        if (level) {
+          const user = await Levels.fetch(message.author.id, message.guild.id);
+          message.channel
+            .send(
+              ` ${message.author.username}, You just leveled up to level ${user.level}!`
+            )
+            .then((m) => m.delete({ timeout: 10000 }));
+        }
+      
+    
+
+
+
+//AFK SYSTEM 
+ //IF YOU  WANT MAKE GLOABAL AFK SYSTEM  JUST  REMOVE SERVER  id
+ //THAKKS FOR WATCHING 
+ //DON'T  FORGET  TO JOIN SUPPORT  SERVER 
+  
+  if (!message.content.startsWith(prefix)) return;
+
+  // If message.member is uncached, cache it.
+  if (!message.member)
+    message.member = await message.guild.fetchMember(message);
+
+  const args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+
+  if (cmd.length === 0) return;
+
+  // Get the command
+  let command = client.commands.get(cmd);
+  // If none is found, try to find it by alias
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+  // If a command is finally found, run the command
+  if (command) command.run(client, message, args);
+});
+
+client.on("message", async message => {
+  if (message.content === "!welcome") {
+    client.emit("guildMemberAdd", message.member);
+  }
+});
+client.on("guildMemberAdd", async member => {
+
+let message = `${member} welcome to ${member.guild.name}`
+
+
+let channel = await client.db.get(`channel_${member.guild.id}`)
+
+if(!channel)return;
+
+client.channels.cache.get(channel).send(message)
+  
+  
+
+
+});
+client.login(process.env.token);
